70ca26ec5eff79eb72e3657bb19158910e56c16f
Ted de Munnik <ta.demunnik@gmail.com>
2023-12-30 20:44:03 +0100

    Split day2 over 2 binaries for the 2 parts

--- day2/src/bin/aoc_2023_day2_part1.rs
+++ day2/src/bin/aoc_2023_day2_part1.rs
@@ -1,81 +1,6 @@
 use std::fs::File;
-use std::io::{BufRead, BufReader};
-use lazy_static::lazy_static;
-use regex::{Regex};
-use thiserror::Error;
-
-#[derive(Debug, Eq, PartialEq)]
-struct DiceCount{
-    red: u32,
-    green: u32,
-    blue: u32
-}
-
-struct Game{
-    id: u32,
-    records: Vec<DiceCount>
-}
-
-#[derive(Error, Debug)]
-enum ParseGameError{
-    #[error("failed to open input file: {inner}")]
-    FailedToOpenInputFile{ inner: std::io::Error },
-    #[error("any")]
-    Any
-}
-
-fn parse_roll(roll: &str) -> DiceCount{
-    let mut record = DiceCount{
-        red: 0,
-        green: 0,
-        blue: 0
-    };
-
-    let entries = roll.split(", ");
-    for entry in entries{
-        let mut segments = entry.split(' ');
-        let test = segments.next().unwrap();
-        let number = test.parse::<u32>().unwrap();
-        let ty = segments.next().unwrap();
-
-        if ty == "red"{
-            record.red += number;
-        }
-        else if ty == "green"{
-            record.green += number;
-        }
-        else if ty == "blue"{
-            record.blue += number;
-        }
-        else{
-            // error
-        }
-    }
-
-    record
-}
-
-fn parse_game(line: &str) -> Result<Game, ParseGameError>{
-    lazy_static!{
-        static ref RE: Regex = Regex::new(r"Game (\d+): (.*)").unwrap();
-    }
-
-    let captures = RE.captures(line).unwrap();
-    let id = captures[1].parse::<u32>().unwrap();
-
-    let records = captures[2].split("; ").map(parse_roll).collect();
-    Ok(Game{ id, records})
-}
-
-fn parse_games<R: BufRead>(reader: R) -> Result<Vec<Game>, ParseGameError>{
-    reader.lines().map(|line| parse_game(&line.map_err(|e| ParseGameError::Any)?)).collect()
-}
-
-fn has_enough_dice(available_dice: &DiceCount, roll: &DiceCount) -> bool{
-    roll.red <= available_dice.red &&
-        roll.green <= available_dice.green &&
-        roll.blue <= available_dice.blue
-}
+use std::io::BufReader;
+use aoc_2023_day2::{Game, ParseGameError, DiceCount, has_enough_dice, parse_games};
 
 fn add_possible_games(games: &[Game], available_dice: &DiceCount) -> u32{
     let possible_games = games.iter().filter(|game| game.records.iter().all(|roll| has_enough_dice(available_dice, roll)));
@@ -104,17 +29,6 @@ fn main(){
 #[cfg(test)]
 mod tests{
     use super::*;
-
-    #[test]
-    fn test_parse_game(){
-        let game = parse_game("Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green").unwrap();
-        assert_eq!(game.id, 1);
-        assert_eq!(game.records, [
-            DiceCount{ blue: 3, red: 4, green: 0 },
-            DiceCount{ red: 1, green: 2, blue: 6 },
-            DiceCount{green: 2, red: 0, blue: 0 },
-        ]);
-    }
 
     #[test]
     fn add_possible_games(){
