b7febae7c4c14b34d3818bbb9305e9f44af767e3
Ted de Munnik <ta.demunnik@gmail.com>
2023-12-31 11:48:29 +0100

    Day 4 part 1

---
+++ day4/src/main.rs
@@ -0,0 +1,95 @@
+use std::{fs::File, io::{BufReader}};
+
+use lazy_static::lazy_static;
+use regex::Regex;
+
+
+
+#[derive(Debug, PartialEq, Eq)]
+struct Card{
+    id: u32,
+    winning: Vec<u8>,
+    have: Vec<u8>
+}
+
+fn parse_number_list(list: &str) -> Vec<u8>{
+    list.split_ascii_whitespace().map(|number| number.parse::<u8>().unwrap()).collect()
+}
+
+fn parse_card(line: &str) -> Card{
+    lazy_static!{
+        static ref RE: Regex = Regex::new(r"Card +(\d+): (.*) \| (.*)").unwrap();
+    }
+
+    let captures = RE.captures(line).unwrap();
+    let id = captures[1].parse::<u32>().unwrap();
+    let winning = parse_number_list(&captures[2]);
+    let have = parse_number_list(&captures[3]);
+    Card{
+        id,
+        winning,
+        have
+    }
+}
+
+fn card_score(card: &Card) -> u32{
+    let count = card.have.iter().filter(|value| card.winning.contains(&value)).count();
+    if count > 0{
+        1 << (count as u32 - 1)
+    }
+    else{
+        0
+    }
+}
+
+fn calculate_total_score<R: std::io::BufRead>(reader: R) -> u32{
+    reader
+        .lines()
+        .map(|line| parse_card(&line.unwrap()))
+        .map(|card| card_score(&card))
+        .sum()
+}
+
+fn read_input() -> Result<impl std::io::BufRead, std::io::Error>{
+    File::open("input_day4.txt")
+        .map(|file| BufReader::new(file))
+}
+
+
+fn main() {
+    aoc_2023_shared::run(read_input()
+        .map(|file| calculate_total_score(file)));
+}
+
+#[cfg(test)]
+mod tests{
+    use indoc::indoc;
+    use crate::{parse_card, Card, calculate_total_score};
+
+    #[test]
+    fn test_parse_card(){
+        const INPUT : &str = "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53";
+        let card = parse_card(INPUT);
+        assert_eq!(card, Card{
+            id: 1,
+            winning: vec![41, 48, 83, 86, 17],
+            have: vec![83, 86, 6, 31, 17, 9, 48, 53]
+        });
+    }
+
+    #[test]
+    fn test_total_score(){
+        const INPUT : &[u8] = indoc!{"
+            Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+            Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+            Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+            Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+            Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+            Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
+        "}.as_bytes();
+
+        let score = calculate_total_score(INPUT);
+        assert_eq!(score, 13);
+    }
+
+}
