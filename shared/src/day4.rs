eb1f2f55871cccad7c70b6e59ca2064130bf012b
Ted de Munnik <ta.demunnik@gmail.com>
2024-01-01 16:03:07 +0100

    Day 4 part 2 solved

--- shared/src/day4.rs
+++ shared/src/day4.rs
@@ -30,8 +30,12 @@ fn parse_card(line: &str) -> Card{
     }
 }
 
+fn card_matches(card: &Card) -> usize{
+    card.have.iter().filter(|value| card.winning.contains(&value)).count()
+}
+
 fn card_score(card: &Card) -> u32{
-    let count = card.have.iter().filter(|value| card.winning.contains(&value)).count();
+    let count = card_matches(card);
     if count > 0{
         1 << (count as u32 - 1)
     }
@@ -54,8 +58,34 @@ fn part1(input: &mut dyn BufRead) {
     crate::run(result);
 }
 
+fn count_scratchcards(input: &mut dyn BufRead) -> Result<usize, std::io::Error>{
+    let cards : Vec<Card> = input.lines().map(|line| parse_card(&line.unwrap())).collect();
+    let mut card_counts : Vec<usize> = vec![1; cards.len()];
+
+    for card_index in 0..cards.len(){
+        let match_count = card_matches(&cards[card_index]);
+
+        let begin_prize = card_index + 1;
+        let end_prize = (begin_prize + match_count).min(cards.len());
+
+        for i in begin_prize..end_prize{
+            card_counts[i] += card_counts[card_index];
+        }
+    }
+
+    Ok(card_counts.iter().copied().sum::<usize>())
+}
+
+#[aoc_2023_markup::aoc_task(2023, 4, 2)]
+fn part2(input: &mut dyn BufRead){
+    crate::run(count_scratchcards(input));
+}
+
+
 #[cfg(test)]
 mod tests{
+    use std::io::Cursor;
+
     use indoc::indoc;
     use super::*;
 
@@ -83,6 +113,22 @@ mod tests{
 
         let score = calculate_total_score(INPUT);
         assert_eq!(score, 13);
+    }
+
+    #[test]
+    fn test_count_cards(){
+        const INPUT : &[u8] = indoc!{"
+            Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+            Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+            Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+            Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+            Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+            Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
+        "}.as_bytes();
+
+        let count = count_scratchcards(&mut Cursor::new(INPUT)).unwrap();
+        assert_eq!(count, 30);
+
     }
 
 }
